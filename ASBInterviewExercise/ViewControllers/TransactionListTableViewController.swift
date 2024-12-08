bcd4514e0caa22804e33b92b910be3a0bd2240d5
kevinylu <kevinylu@live.com>
2024-12-08 15:54:16 +1300

    Apply Custom table cell display transection
    Change Main Storyboard TableViewCell Identifier
    Change Main Storyboard TableViewCell Classname
    Implement TransactionTableViewCell configure with transactionViewModel

--- ASBInterviewExercise/ViewControllers/TransactionListTableViewController.swift
+++ ASBInterviewExercise/ViewControllers/TransactionListTableViewController.swift
@@ -41,4 +41,33 @@ class TransactionListTableViewController: UITableViewController {
             }
         }
     }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.transactionListViewModel == nil ? 0 : self.transactionListViewModel.numberOfSections
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.transactionListViewModel.numberOfRowsInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TransactionTableViewCell", for: indexPath) as? TransactionTableViewCell else {
+            fatalError("TransactionTableViewCell not found")
+        }
+        
+        let transactionViewModel = self.transactionListViewModel.transactionAtIndex(indexPath.row)
+        cell.configure(with: transactionViewModel)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Get the selected transaction view model
+        let selectedViewModel = transactionListViewModel.transactionAtIndex(indexPath.row)
+        
+        // Create the TransactionDetailsViewController with the selected view model
+        let detailsViewController = TransactionDetailsViewController()
+        
+        // Push it onto the navigation stack
+        navigationController?.pushViewController(detailsViewController, animated: true)
+    }
 }
